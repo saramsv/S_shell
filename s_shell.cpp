ce1f6a109bd0220325e6906fb3f2ae0e67b72ca6
saramsv <sara.mousavi.ch@gmail.com>
2016-02-22 01:19:40 -0600

    Changing ofstream to FILE *

--- s_shell.cpp
+++ s_shell.cpp
@@ -9,41 +9,69 @@ string location="root@sara / $ ";
 int file_system_size = 100*1024*1024;
 int inode_max_number_of_files = 1024;
 int n_inodes = 0;
+int block_siz = 2048;
 
+FILE* f_open(int truncate=0)
+{
+    FILE *fp;
+    if (truncate)
+    {
+        fp = fopen("/home/sara/s_shell/my_filesys.bin", "wb");
+    }
+    else
+    {
+        fp = fopen("/home/sara/s_shell/my_filesys.bin", "rb+");
+    }
+    return fp;
+}
 
 void add_to_inode_table(short inode_number, int offset, char type)
 {
     
-    ofstream fp;
-    fp.open("my_filesys.bin", ios::in| ios::out | ios::binary | ios::ate);
-    fp.seekp(n_inodes*8 , ios::beg);
-    fp.write((char *)&inode_number,sizeof(inode_number));
-    fp.write((char *)&offset,sizeof(offset));
-    fp.write((char *)&type,sizeof(type));
+    FILE * fp = f_open();
+   // fp.open("my_filesys.bin", ios::in| ios::out | ios::binary | ios::ate);
+//    fp.seekp(n_inodes*8 , ios::beg);
+    fseek(fp, n_inodes * 8 , SEEK_SET);
+    fwrite((char *)&inode_number, sizeof(short), 1, fp);
+    fwrite((char *)&offset, sizeof(int), 1, fp);
+    fwrite((char *)&type, sizeof(char), 1, fp);
+    // fp.write((char *) &offset, sizeof(offset));
+    // fp.write((char *) &type, sizeof(type));
     n_inodes += 1;
-    fp.close();
+    fclose(fp);
 }
 
 void make_a_header(string filename, short inode_number)
 {
-    ofstream fp("my_filesys.bin", ios::out | ios::in | ios::binary | ios::ate); 
-    fp.seekp(inode_max_number_of_files * 8, ios::beg);
-    fp.write(filename.c_str(), filename.size());
-    fp.write((char *)&inode_number, sizeof(inode_number));
-    fp.close();
+    FILE * fp = f_open();
+    fseek(fp, inode_max_number_of_files * 8, SEEK_SET);
+    fwrite(filename.c_str(), sizeof(char), sizeof(filename), fp);
+    fwrite(&inode_number, sizeof(inode_number), 1, fp);
+    cout << "sara!!";
+    fclose(fp);
 }
 
 void mkfs()
 {
-    ofstream fp;
+    /*ofstream fp;
     fp.open("my_filesys.bin", ios::out |ios::in| ios::binary | ios::ate); 
     fp.seekp(file_system_size - 1, ios::beg);
     fp<<'0';
     fp.close();
-    make_a_header("/",1);
+    make_a_header("/",1);*/
+
+    FILE *fp = f_open(1);
+    fseek(fp, file_system_size-1, SEEK_SET);
+    char c = '0';
+    fwrite(&c, sizeof(char), 1, fp);
+    short inode_number = 1;
+    fclose(fp);
+
+
+    make_a_header("/", inode_number);
     add_to_inode_table(1,8*inode_max_number_of_files,'d');
-    cout<<"My File System!"<<"\n";
-    cout<<location;
+    cout << "My File System!" << "\n";
+    cout << location;
 }
 
 void display()
